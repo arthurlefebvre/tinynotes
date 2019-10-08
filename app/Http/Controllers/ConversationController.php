87ca5172ed67de343d52cf68764723d8d8ee9cec
beubarrousse <arthur.lefebvre94@gmail.com>
2019-10-08 15:03:27 +0200

    Removed debbug code in ConversationController@deleteMessage

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -136,11 +136,6 @@ class ConversationController extends Controller
 
     public function deleteMessage(Request $request)
     {
-
-        return response()->json([
-            'status' => 200,
-            'content' => $request['id']
-        ]);
         $deleteMessage = Message::destroy($request['id']);
 
         if ($deleteMessage) {
