12090a799d7dac0bf70a11f84e2aa7b8cceac75f
beubarrousse <arthur.lefebvre94@gmail.com>
2019-10-08 18:41:43 +0200

    debug conversation controller

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -27,7 +27,6 @@ class ConversationController extends Controller
 
     public function create(Request $request)
     {
-	return response()->json(['status' => 200, 'content' => $request['name'] ]);
         if ($request['name'] === null) {
 
             return response()->json([
@@ -45,10 +44,17 @@ class ConversationController extends Controller
                 $request['userId'], Auth::user()->id
             ]);
 
-            return response()->json([
-                'status' => 200,
-                'id' => $conversation->id
-            ]);
+            if ($conversation) {
+                return response()->json([
+                    'status' => 200,
+                    'id' => $conversation->id
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'content' => $conversation
+                ]);
+            }
         }
     }
 
