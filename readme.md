b2cb8259c6e6afb40ee080be84c016df3a85b883
ArthurLEFEBVRE <arthur.lefebvre94@gmail.com>
2019-11-14 21:42:53 +0100

    Added images in public/ folder, updated Readme.md file, and updated home.blade.php to add step by step guide

--- readme.md
+++ readme.md
@@ -2,10 +2,8 @@
 
 A small projet to discuss with your friends via post-it like messages.
 
-https://tinynotes.alefebvre.dev/
-
 Example:
-![alt text](tinynotes_example.png "Post-it message example screenshot")
+![alt text](public/tinynotes.png 'Post-it message example screenshot')
 
 Uses Laravel 5.8
 
@@ -13,12 +11,14 @@ Uses Laravel 5.8
 
 (requires composer and npm) <br>
 
--   Create a .env file with your database configuration (see https://github.com/laravel/laravel/blob/master/.env.example) at the root of the project.
 -   Install Php dependencies:
     `composer install` <br>
 -   Install Javascript dependencies :
     `npm install`
 -   Update your database tables :
     `php artisan migrate`
+-   Create a .env file with your database configuration (see https://github.com/laravel/laravel/blob/master/.env.example) at the root of the project.
+-   Generate your application key :
+    `php artisan key:generate`
 -   Run your local server :
     `php artisan serve`
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container text-center">
+<div class="container-fluid">
+        <div class="text-center">
             <h3 class="display-4">
                 {{ config('app.name', 'TinyNotes') }}
             </h3>
 
-            <div class="alert alert-warning" style="display:inline-block; background-color: #ffff88!important;">
+            <div class="alert alert-warning" style="display:inline-block; background-color: #ffff88!important;margin-bottom: 70px;" role="alert">
                 <h5>TinyNotes est un site qui vous permet d'échanger avec un proche grâce à des Post-Its ! <br>
                 Faites vous des bisous papiers !
                 </h5>
                 <br>
                 <a class="btn btn-primary" href="{{ route('conversation.index') }}">{{ __('Démarrer une conversation') }}</a>
             </div>
+            <h2>Etape par étape : </h2>
+        </div>
+
+
+        <div class="alert" style="display:inline-block; background-color: lightblue!important; width: 40%;
+                border-bottom-right-radius: 60px 10px;
+                box-shadow: 5px 5px 7px rgba(33, 33, 33, 0.7);" role="alert">
+            <h3>Inscrivez-vous :</h3>
+            <img src="/signuppic.png" style="transform: rotate(-5deg);float:left; width:50%; height: 70%"/>
+            <h5 style="text-align: justify;">Pour vous inscrire, remplissez simplement le formulaire ! Vous pouvez aussi vous connecter directement grâce à votre compte Gmail.
+            </h5>
+        </div>
+        <br>
+        <div class="alert" style="display:inline-block; background-color: lightgreen!important; 
+                width: 40%; margin-left: 60%;border-bottom-right-radius: 60px 10px;
+                box-shadow: 5px 5px 7px rgba(33, 33, 33, 0.7);" role="alert">
+            <h3>Créez une conversation : </h3>
+            <br>
+            <img src="/userspic.png" style="transform: rotate(5deg);float:right; width:70%; height: 30%">
+            <h5 style="text-align: justify;">Recherchez une personne par son adresse emaill, puis lancer la conversation.
+            </h5>
+        </div>
+        <br>
+        <br>
+        <div class="text-center">
+    
+            <div class="alert" style="display:inline-block; background-color: lightcoral!important; width:40%;
+                    border-bottom-right-radius: 60px 10px;
+                    box-shadow: 5px 5px 7px rgba(33, 33, 33, 0.7);" role="alert">
+                <h3>Discutez !</h3>
+                <img src="/tinynotes.png" style="width:80%; height: 80%">
+                <br>
+                <h5>Maintenant votre conversation créée, vous n'avez plus qu'à discuter par Post-its !
+                </h5>
+            </div>
+        </div>
 
 
 </div>
